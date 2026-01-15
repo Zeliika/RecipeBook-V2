f14bbe2f300bfab588f63a9cc1e06acf2e5934ef
Hannah Boeckers <hannah.boeckers@web.de>
2026-01-15 15:49:11 +0100

    Remove TabHeaderContainer, Change TabPosition of TabContentContainer to bottom

--- scripts/display/App.cs
+++ scripts/display/App.cs
@@ -7,7 +7,6 @@ public partial class App : VSplitContainer
     protected EventBus eventBus;
 
     [Export] protected TabContainer TabContentContainer;
-    [Export] protected TabBar TabHeaderContainer;
     public PackedScene RecipeScene = GD.Load<PackedScene>("uid://cqpd148u0ridy");
 
     public override void _Ready()
@@ -19,21 +18,23 @@ public partial class App : VSplitContainer
 
     protected void OnRecipeOpened(RecipeData data, bool editMode)
     {
-        for (int tab = 1; tab < TabHeaderContainer.TabCount; tab++)
+        foreach (Node recipeDisplay in TabContentContainer.GetChildren())
         {
-            if (TabHeaderContainer.GetTabTitle(tab) == data.recipeName)
+            if (recipeDisplay is not RecipeDisplay)
             {
-                TabContentContainer.CurrentTab = tab;
-                TabHeaderContainer.CurrentTab = tab;
+                continue;
+            }
+            if (TabContentContainer.GetTabTitle(recipeDisplay.GetIndex()) == data.recipeName)
+            {
+                TabContentContainer.CurrentTab = recipeDisplay.GetIndex();
                 return;
             }
         }
         RecipeDisplay recipe = (RecipeDisplay)RecipeScene.Instantiate();
         TabContentContainer.AddChild(recipe);
+        TabContentContainer.SetTabTitle(recipe.GetIndex(), data.recipeName);
         recipe.Init(data);
-        TabHeaderContainer.AddTab(data.recipeName);
-        TabHeaderContainer.CurrentTab = TabHeaderContainer.TabCount - 1;
-        TabContentContainer.CurrentTab = TabHeaderContainer.TabCount - 1;
+        TabContentContainer.CurrentTab = TabContentContainer.GetChildCount() - 1;
         if (editMode == true)
         {
             recipe.SetEditMode(true, data);
@@ -43,7 +44,6 @@ public partial class App : VSplitContainer
     protected void OnRecipeClosed()
     {
         TabContentContainer.GetChild(TabContentContainer.CurrentTab).QueueFree();
-        TabHeaderContainer.RemoveTab(TabHeaderContainer.CurrentTab);
     }
 
     protected void OnTabSelected(int tab)
